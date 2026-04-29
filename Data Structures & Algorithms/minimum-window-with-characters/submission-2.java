c1c45243651457ce5db963cb08ebc3f878b0f4cf
PondWhisper <3820228386@qq.com>
2026-04-29 13:19:29 +0800

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.java
@@ -0,0 +1,46 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s.length() < t.length()) {
+            return "";
+        }
+
+        int[] tCount = new int[58];
+        int[] sCount = new int[58];
+        String result = "";
+        int min = Integer.MAX_VALUE;
+
+        for (char c : t.toCharArray()) {
+            tCount[c - 'A']++;
+        }
+        int count = 0;
+        for (int j : tCount) {
+            count += (j > 0) ? 1 : 0;
+        }
+
+        int matchCount = 0;
+        int l = 0;
+
+        for (int r = 0; r < s.length(); r++) {
+            int index = s.charAt(r) - 'A';
+            if (++sCount[index] == tCount[index] && tCount[index] != 0) {
+                matchCount++;
+            }
+            index = s.charAt(l) - 'A';
+            while (sCount[index] > tCount[index] || tCount[index] == 0) {
+                sCount[index]--;
+                if (l == r) {
+                    break;
+                }
+                index = s.charAt(++l) - 'A';
+            }
+            if (matchCount == count) {
+                int currLength = r - l + 1;
+                if (currLength < min) {
+                    min = currLength;
+                    result = s.substring(l, r + 1);
+                }
+            }
+        }
+        return result;
+    }
+}
