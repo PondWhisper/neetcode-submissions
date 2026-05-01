e8072bd7076cf8e2a4be73da407e1995bcca299a
PondWhisper <3820228386@qq.com>
2026-05-01 21:34:03 +0800

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,25 @@
+
+
+class Solution {
+    public boolean isValid(String s) {
+        HashMap<Character, Character> map = new HashMap<>();
+        Deque<Character> stack = new ArrayDeque<>();
+
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+
+        for(char c:s.toCharArray()){
+            if(map.containsKey(c)){
+                if(!stack.isEmpty()&&stack.peek()==map.get(c)){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+            }else{
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
