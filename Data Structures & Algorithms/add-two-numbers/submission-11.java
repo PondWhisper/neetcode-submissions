83464bcd7d9ebff481d7ab8ad8ea1a5ed9b6d93d
PondWhisper <3820228386@qq.com>
2026-05-12 16:55:15 +0800

    Add: add-two-numbers - submission-11

---
+++ Data Structures & Algorithms/add-two-numbers/submission-11.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode(0);
+        ListNode curr = res;
+
+        //这里要着重看别人是怎么解决这个问题的,当merge两个linkedlist但是两个list长度不一样
+        int addNum = 0;
+        while (l1 != null || l2 != null || addNum != 0) {
+            int v1 = l1 != null ? l1.val : 0;
+            int v2 = l2 != null ? l2.val : 0;
+
+            int sum = v1 + v2 + addNum;
+            addNum = sum / 10;
+
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        return res.next;
+    }
+}
