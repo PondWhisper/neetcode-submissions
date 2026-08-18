56cbab4f50d886586bb9b3a342cccdcdbfb64bf3
PondWhisper <3820228386@qq.com>
2026-08-18 10:41:23 +0800

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+class Solution {
+public:
+  int climbStairs(int n) {
+    std::vector<int> stairs;
+    stairs.reserve(n + 1);
+    stairs[0] = 1, stairs[1] = 1;
+    for (int i = 2; i <= n; ++i) {
+      stairs[i] = stairs[i - 1] + stairs[i - 2];
+    }
+
+    return stairs[n];
+  }
+};
